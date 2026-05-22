3afb98d066befab963965ec25dc466d141e51072
Adrian Ocneanu <aocneanu@gmail.com>
2026-05-22 09:31:19 +0300

    Allow empty mail signature regards

--- resources/views/vendor/mail/html/signature.blade.php
+++ resources/views/vendor/mail/html/signature.blade.php
@@ -1,12 +1,18 @@
 @php
     $content = trim((string) $slot);
+    $regards = trim(__('laravel-enso/mails::signature.regards'));
+    $team = trim(__('laravel-enso/mails::signature.team', ['brand' => config('enso.mails.brand.name')]));
 @endphp
 
 <div style="color: {{ config('enso.mails.text.muted') }}; font-size: 12px; line-height: 1.5; margin-top: 10px;">
 @if($content === '')
 <p style="color: {{ config('enso.mails.text.body') }}; font-size: 12px; line-height: 1.5; margin: 0;">
-{{ __('laravel-enso/mails::signature.regards') }}<br>
-<strong>{{ __('laravel-enso/mails::signature.team', ['brand' => config('enso.mails.brand.name')]) }}</strong>
+@if($regards !== '')
+{{ $regards }}<br>
+@endif
+@if($team !== '')
+<strong>{{ $team }}</strong>
+@endif
 </p>
 @else
 {{ Illuminate\Mail\Markdown::parse($slot) }}
--- resources/views/vendor/mail/text/signature.blade.php
+++ resources/views/vendor/mail/text/signature.blade.php
@@ -1,10 +1,16 @@
 @php
     $content = trim((string) $slot);
+    $regards = trim(__('laravel-enso/mails::signature.regards'));
+    $team = trim(__('laravel-enso/mails::signature.team', ['brand' => config('enso.mails.brand.name')]));
 @endphp
 
 @if($content === '')
-{{ __('laravel-enso/mails::signature.regards') }}
-{{ __('laravel-enso/mails::signature.team', ['brand' => config('enso.mails.brand.name')]) }}
+@if($regards !== '')
+{{ $regards }}
+@endif
+@if($team !== '')
+{{ $team }}
+@endif
 @else
 {{ $slot }}
 @endif
